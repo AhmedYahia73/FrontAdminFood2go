752bc69831feec80241632f90524138401db9f75
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-08-30 17:04:41 +0300

    fdsfd

--- src/Pages/Dashboard/Admin/Orders/DetailsOrder/DetailsOrderPage.jsx
+++ src/Pages/Dashboard/Admin/Orders/DetailsOrder/DetailsOrderPage.jsx
@@ -1052,7 +1052,7 @@ const DetailsOrderPage = () => {
                           // Product is a single object → NOT an array
                           const product = orderDetail.product;
                           if (product) {
-                            totalItemPrice += (product.price_after_discount ? product.price_after_discount : product.price_after_tax ? product.price_after_tax : product.price || 0) * (product.count || 1);
+                            totalItemPrice += (product.price_after_discount ? product.price_after_discount : product.price || 0) * (product.count || 1);
                           }
 
                           // Inside your total calculation loop
